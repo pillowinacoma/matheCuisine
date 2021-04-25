ded6438e35be995863a36a27bb20b389b49d4fe7
Nicolas DA COSTA <nicolas.da-costa@nidc.fr>
2021-04-25 23:01:08 +0200

    maj model generic

--- client/src/pages/components/models.tsx
+++ client/src/pages/components/models.tsx
@@ -1,29 +1,53 @@
 import * as React from 'react';
-import { useLoader } from 'react-three-fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { useLoader, useFrame, applyProps, RenderCallback } from 'react-three-fiber';
+import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 
 
 import { Suspense } from 'react';
 import { Vector3 } from 'three';
 
-export const Model = (props:{file: string, position: [number, number, number]}) => {
+export const Model = (props:{file: string, position?: [number, number, number],
+  scale?: [number, number, number],
+  onPointerDown?: ((event: any) => void) | undefined,
+  onPointerCancel?: ((event: any) => void) | undefined,
+  onPointerMove?: ((event: any) => void) | undefined,
+  onPointerOut?: ((event: any) => void) | undefined,
+  onPointerLeave?: ((event: any) => void) | undefined,
+  onPointerMissed?: ((event: any) => void) | undefined,
+  onPointerOver?: ((event: any) => void) | undefined,
+  onPointerUp?: ((event: any) => void) | undefined,
+  onClick?: ((event: any) => void) | undefined,
+  onDoubleClick?: ((event: any) => void) | undefined,
+  onWheel?: ((event: any) => void) | undefined,
+  frame?: (gltf: GLTF) => void | undefined 
+}) => {
 
     const [pos, setPos] = React.useState(props.position);
     const glb = '/models/'+props.file+'.glb';
-    
+    var group = React.useRef();
     const gltf = useLoader(GLTFLoader, glb);
 
-    const group = React.useRef();
 
-    //console.log(gltf.nodes.Cube);
-
+    useFrame(() => {
+      if(props.frame != undefined) 
+        props.frame(gltf);
+    });
     return (
       <group ref={group} {...props}
-        onPointerDown = {(e) => {
-        console.log(e);
-      }}
+        onPointerDown = {props.onPointerDown}
+        onPointerCancel = {props.onPointerCancel}
+        onPointerMove = {props.onPointerMove}
+        onPointerOut = {props.onPointerOut}
+        onPointerEnter = {props.onPointerOut}
+        onPointerLeave = {props.onPointerLeave}
+        onPointerMissed = {props.onPointerMissed}
+        onPointerOver = {props.onPointerOver}
+        onPointerUp = {props.onPointerUp}
+        onClick = {props.onClick}
+        onDoubleClick = {props.onDoubleClick}
+        onWheel = {props.onWheel}
       >
-        <primitive object={gltf.scene} position={pos}/>
+        <primitive object={gltf.scene} position={pos ? pos : [0,0,0]} scale={props.scale ? props.scale : [1,1,1]}/>
       </group>
     )
 }
--- client/src/pages/exercices/type1.tsx
+++ client/src/pages/exercices/type1.tsx
@@ -4,17 +4,20 @@ import Board from '../components/board';
 import {Model} from '../components/models';
 import { Suspense } from 'react';
 import { Vector3 } from 'three';
+import { RenderCallback } from 'react-three-fiber';
 
 const Type1 = () : JSX.Element => {
 
 
-
+    const frame = (gltf: any) =>  {
+        gltf.scene.rotateY(0.03);   
+    }
 
 
     return (
         <Board>
                 <Suspense fallback={'loading'}>
-                    <Model file={"banana"} position={[0,0,0]}/>
+                    <Model file={"banana"} position={[0,0,0]} scale={[.75,.75,.75]} frame={frame}/>
                 </Suspense>
         </Board>
     );
