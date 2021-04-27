a3812dccc4bceb1e6723be518fa51408f17c0b80
Nicolas DA COSTA <nicolas.da-costa@nidc.fr>
2021-04-27 11:55:32 +0200

    maj ex type1

--- client/src/pages/exercices/type1.tsx
+++ client/src/pages/exercices/type1.tsx
@@ -8,8 +8,18 @@ import { RenderCallback } from 'react-three-fiber';
 
 const Type1 = () : JSX.Element => {
 
+    const vars: Array<number> = [12,4,3];
+    var coefResponse = 1;
+    var response = -9999999;
+    var eq = 0;
+    vars.forEach(element => {
+        eq += element;
+    });
+
+    eq += coefResponse * response;
 
 
+    var resultat = 16;
 
     const frame = (gltf: any) =>  {
         gltf.scene.rotateY(0.03);   
