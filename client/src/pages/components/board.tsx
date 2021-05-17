d404d6f877834281b2ade902f486a0895a08d0c1
Nicolas DA COSTA <nicolas.da-costa@nidc.fr>
2021-05-17 12:23:55 +0200

    correction style

--- client/src/pages/components/board.tsx
+++ client/src/pages/components/board.tsx
@@ -4,13 +4,10 @@ import { makeStyles } from "@material-ui/core";
 
 
 const useStyle = makeStyles((theme) => ({
-
-    "makeStyles-canvas-26": {
-        position: "absolute",
-        height: "calc(100vh - 64px)"
-    },
     canvas:{
-        
+        position: "absolute",
+        height: "calc(100vh - 64px)",
+        width: "100%"
     }
 
 }));
@@ -20,11 +17,14 @@ const Board = (props: { children: any; camera?: any | undefined }) => {
     const classes = useStyle();
 
     return (
-        <Canvas camera={props.camera ? props.camera : { position: [0, 0, 10] }} className={classes.canvas}>
-            <ambientLight intensity={0.5} />
-            <spotLight intensity={0.8} position={[300, 300, 400]} />
-            {props.children}
-        </Canvas>
+        <div className={classes.canvas}>
+            <Canvas camera={props.camera ? props.camera : { position: [0, 0, 10] }} >
+                <ambientLight intensity={0.5} />
+                <spotLight intensity={0.8} position={[300, 300, 400]} />
+                {props.children}
+            </Canvas>
+        </div>
+
     );
 };
 
