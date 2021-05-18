4a7eb0b463d020163857117273605333918b0b03
Aziz <abdelazizsbaai@gmail.com>
2021-05-18 15:07:25 +0200

    page déconnection

--- client/src/pages/connection.tsx
+++ client/src/pages/connection.tsx
@@ -1,38 +1,37 @@
-import * as React from 'react';
-import { Button, TextField, makeStyles } from '@material-ui/core';
-import {LangContext} from '../engine/translation/i18n';
-import {UserContext} from '../engine/profile/profile'
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { Button, TextField, makeStyles } from "@material-ui/core";
+import { LangContext } from "../engine/translation/i18n";
+import { UserContext } from "../engine/profile/profile";
+import { Link } from "react-router-dom";
 
 const useStyle = makeStyles((theme) => ({
     page: {
         width: 400,
         margin: "auto",
         marginTop: 100,
-        display:"grid",
+        display: "grid",
         padding: 30,
         backgroundColor: "whitesmoke",
-        borderRadius: "20px"
+        borderRadius: "20px",
     },
     button: {
         margin: "auto",
-        marginTop: 15
-    }
+        marginTop: 15,
+    },
 }));
 
 const Connection = () => {
-
     const classes = useStyle();
 
-    const {translate} = React.useContext(LangContext);
-    const {setLogin} = React.useContext(UserContext);
+    const { translate } = React.useContext(LangContext);
+    const { setLogin } = React.useContext(UserContext);
 
     const [login, changeLogin] = React.useState("");
     const [error] = React.useState("");
-    
+
     const handleConnexion = () => {
         setLogin(login);
-    }
+    };
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         changeLogin(event.target.value);
@@ -42,13 +41,27 @@ const Connection = () => {
         <div className={classes.page}>
             <h1>Bienvenue sur MatheCuisine</h1>
             <h2>Pour continuer sur notre site veuillez vous identifier</h2>
-            <TextField id="standard-basic" label={translate("loginDetail")} onChange={handleChange} autoComplete="off" placeholder="jean" error={error !== "" ? true : false } helperText={error !== "" ? error : ""}/>
-            <Button variant="contained" color="primary" onClick={handleConnexion} component={Link} to='/' className={classes.button}>
-                {translate('connection')}
+            <TextField
+                id="standard-basic"
+                label={translate("loginDetail")}
+                onChange={handleChange}
+                autoComplete="off"
+                placeholder="jean"
+                error={error !== "" ? true : false}
+                helperText={error !== "" ? error : ""}
+            />
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={handleConnexion}
+                component={Link}
+                to="/"
+                className={classes.button}
+            >
+                {translate("connection")}
             </Button>
         </div>
     );
-
-}
+};
 
 export default Connection;
--- client/src/pages/disconnect.tsx
+++ client/src/pages/disconnect.tsx
@@ -1,28 +1,53 @@
-import * as React from 'react';
-import { Button } from '@material-ui/core';
-import {LangContext} from '../engine/translation/i18n';
-import {UserContext} from '../engine/profile/profile';
-import {Link} from 'react-router-dom';
+import * as React from "react";
+import { Button, makeStyles } from "@material-ui/core";
+import { LangContext } from "../engine/translation/i18n";
+import { UserContext } from "../engine/profile/profile";
+import { Link } from "react-router-dom";
 
+const useStyle = makeStyles((theme) => ({
+    page: {
+        width: 400,
+        margin: "auto",
+        marginTop: 100,
+        display: "grid",
+        padding: 30,
+        backgroundColor: "whitesmoke",
+        borderRadius: "20px",
+    },
+    button: {
+        margin: "auto",
+        marginTop: 15,
+    },
+}));
 
 const Disconnection = () => {
+    const { translate } = React.useContext(LangContext);
+    const { deleteLogin } = React.useContext(UserContext);
 
-    const {translate} = React.useContext(LangContext);
-    const {deleteLogin} = React.useContext(UserContext);
-    
     const handleConnexion = () => {
         deleteLogin();
-    }
+    };
 
+    const classes = useStyle();
     return (
-        <div>
-            <h1>{translate("toDisconnect")}</h1>
-            <Button variant="contained" color="primary" onClick={handleConnexion} component={Link} to='/'>
-                {translate('clickHere')}
+        <div className={classes.page}>
+            <h1>Au revoir</h1>
+            <h4>
+                Vous pouvez vous déconnecter en cliquant sur le boutton si
+                dessous
+            </h4>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={handleConnexion}
+                component={Link}
+                className={classes.button}
+                to="/"
+            >
+                {translate("clickHere")}
             </Button>
         </div>
     );
-
-}
+};
 
 export default Disconnection;
